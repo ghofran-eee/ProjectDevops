67c12d194fecc161c3eb3ef089209a6b64054063
Ghofrane OUELHAZI <GOuelhazi@somezzo.com>
2026-01-24 15:15:36 +0100

    Update fichier test

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -3,14 +3,11 @@ package tn.esprit.studentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
-        assertTrue(true);
     }
 
 }
