e2c54de6572a38e71d0881beaac090517ba0fd43
Ghofrane OUELHAZI <GOuelhazi@somezzo.com>
2026-02-03 22:16:52 +0100

    update class TestUnitaire

--- src/test/java/tn/esprit/studentmanagement/TestUnitaire.java
+++ src/test/java/tn/esprit/studentmanagement/TestUnitaire.java
@@ -81,10 +81,10 @@ public class TestUnitaire {
         Student result = studentService.getStudentById(1L);
 
         // Assert
-        assertNotNull(result);   
+        assertNotNull(result);
         //assertEquals("Ouelhazi", result.getFirstName());
         assertEquals(student1, result);
-        verify(studentRepository, times(1)).findById(1L);
+        verify(studentRepository, times(1)).findById(1L);    
     }
 
 }
